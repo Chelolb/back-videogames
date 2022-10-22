88db59be8e875438f58ac60fb5023aad9485fdd4
Chelolb <marcelolitwin@gmail.com>
2022-10-22 08:01:21 -0300

    update for render1

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const axios = require('axios');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URI
+  DB_URI
 } = process.env;
 
 //  Conexion con DB PostgreSQL alojada en servidor externo
